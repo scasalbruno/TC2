1ebc4d76f934a4bd6297bd8519e66a21565a12b4
Sebastian Casal Bruno <scasalbruno@users.noreply.github.com>
2022-05-06 16:55:53 -0300

    Revert "ts1 error merge"
    This reverts commit 31fea450ab510773b276273ff68c49b56b0df4fc, reversing
    changes made to 48bfa6c453622680c47b49ca5171fc65dd613669.

--- TPS/TPS_1/Simulacion TPs_1.py
+++ TPS/TPS_1/Simulacion TPs_1.py
@@ -1,15 +1,3 @@
 # -*- coding: utf-8 -*-
-
-import matplotlib.pyplot as plt
 import numpy as np
-import scipy.signal as sig
-from splane import analyze_sys, pretty_print_bicuad_omegayq
 
-num = np.array([1,-1]) 
-den = np.array([1,1])
-
-pretty_print_bicuad_omegayq(num,den)
-tsf = sig.TransferFunction(num,den)
-
-plt.close('all')
-analyze_sys(tsf, 'Respuesta del pasatodo')
